cdb68d09f5f09970bc4f9179f6bac5d459346b9b
Robert Impey <robert-impey@users.noreply.github.com>
2023-06-23 22:22:03 +0100

    LA: Dot product of vectors

--- vectors/vectors.go
+++ vectors/vectors.go
@@ -24,7 +24,7 @@ func main() {
 	a := []float64{2, 4}
 	b := []float64{3, 1}
 
-	sumOfAAndB := make([]float64, 2)
+	sumOfAAndB := make([]float64, len(a))
 
 	for i, elementOfA := range a {
 		elementOfB := b[i]
@@ -35,4 +35,27 @@ func main() {
 	for _, elementOfSum := range sumOfAAndB {
 		fmt.Println(elementOfSum)
 	}
+
+	// Dot product of two vectors
+	us := []float64{4, -2, 0, 1}
+	vs := []float64{-1, -3, 1, 5}
+
+	dotProductItems := make([]float64, len(us))
+
+	for i, u := range us {
+		v := vs[i]
+		dotProductItems[i] = u * v
+	}
+
+	fmt.Println("The dot product items of 2 vectors")
+	for _, elem := range dotProductItems {
+		fmt.Println(elem)
+	}
+
+	dotProduct := 0.0
+	for _, elem := range dotProductItems {
+		dotProduct += elem
+	}
+
+	fmt.Printf("The dot product: %f\n", dotProduct)
 }
