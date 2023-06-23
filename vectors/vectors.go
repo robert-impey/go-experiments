f0373a0476a460bf6ee79de74959379988820fd2
robert-impey <robert-impey@users.noreply.github.com>
2023-06-23 16:48:17 +0100

    LA: Sum of vectors

--- vectors/vectors.go
+++ vectors/vectors.go
@@ -19,4 +19,20 @@ func main() {
 	mag := math.Sqrt(sumOfSquares)
 
 	fmt.Println(mag)
+
+	// Sum of Vectors (page 28)
+	a := []float64{2, 4}
+	b := []float64{3, 1}
+
+	sumOfAAndB := make([]float64, 2)
+
+	for i, elementOfA := range a {
+		elementOfB := b[i]
+		sumOfAAndB[i] = elementOfA + elementOfB
+	}
+
+	fmt.Println("The sum of A and B")
+	for _, elementOfSum := range sumOfAAndB {
+		fmt.Println(elementOfSum)
+	}
 }
