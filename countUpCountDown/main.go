c7091b8bc25d1cd13e32c64b63cf8ac5bfa443e2
Robert Impey <robert-impey@users.noreply.github.com>
2024-12-06 20:17:58 +0000

    CUCD: Flags for params

--- countUpCountDown/main.go
+++ countUpCountDown/main.go
@@ -1,27 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
-var annPlace = 2.0
-var benPlace = 4.0
+var p1Name = flag.String("p1", "Player 1", "Player 1 Name")
+var p2Name = flag.String("p2", "Player 2", "Player 2 Name")
+
+var p1Start = flag.Float64("p1st", 2.0, "Player 1 start")
+var p2Start = flag.Float64("p2st", 4.0, "Player 2 Start")
+
+var p1Step = flag.Float64("p1sp", 1.0/2, "Player 1 Step")
+var p2Step = flag.Float64("p2sp", 1.0/8, "Player 2 Step")
 
 var numSteps = 0
 
 func main() {
-	var annStep = 1.0 / 2
-	var benStep = 1.0 / 8
+	flag.Parse()
 
-	for ; annPlace <= benPlace; numSteps++ {
-		printState()
-		annPlace += annStep
-		benPlace -= benStep
+	var p1Place = *p1Start
+	var p2Place = *p2Start
+
+	for ; p1Place <= p2Place; numSteps++ {
+		printState(&p1Place, &p2Place)
+		p1Place += *p1Step
+		p2Place -= *p2Step
 	}
 
 	fmt.Println("They have passed each other!")
-	
-	printState()
+
+	printState(&p1Place, &p2Place)
 }
 
-func printState() {
-	fmt.Printf("After %d steps, Ann is at %f and Ben is at %f\n", numSteps, annPlace, benPlace)
+func printState(p1Place, p2Place *float64) {
+	fmt.Printf("After %d steps, %s is at %f and %s is at %f\n",
+		numSteps, *p1Name, *p1Place, *p2Name, *p2Place)
 }
