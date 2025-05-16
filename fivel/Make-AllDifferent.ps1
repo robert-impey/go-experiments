3dbd9199c813f6889307c90a8c61b3c2b8cc18c6
Robert Impey <robert-impey@users.noreply.github.com>
2025-05-16 10:54:08 +0100

    Go: FL: Adjusted wrapper to work on Linux

--- fivel/Make-AllDifferent.ps1
+++ fivel/Make-AllDifferent.ps1
@@ -4,14 +4,16 @@ param($exe, $inputDir, $outputDir)
 
 # E.g. 
 # > .\Make-AllDifferent.ps1 fivel.exe $env:DATA $env:DATA\FiveLetters\Go
+# or
+# > ./Make-AllDifferent.ps1 "$($pwd.Path)/fivel" $env:DATA $env:DATA/FiveLetters/Go
 
-$dictionaryFile = "$($inputDir)\british-english.txt"
+$dictionaryFile = "$($inputDir)/british-english.txt"
 
-$fiveLetterWordsFile = "$($outputDir)\five-letter-words.txt"
+$fiveLetterWordsFile = "$($outputDir)/five-letter-words.txt"
 Start-Process $exe -NoNewWindow -Wait -ArgumentList 'find5LetterWords' -RedirectStandardInput $dictionaryFile -RedirectStandardOutput $fiveLetterWordsFile
 
-$allLatinFile = "$($outputDir)\five-letter-words-all-latin.txt"
+$allLatinFile = "$($outputDir)/five-letter-words-all-latin.txt"
 Start-Process $exe -NoNewWindow -Wait -ArgumentList 'removeWordsWithNonLatin' -RedirectStandardInput $fiveLetterWordsFile -RedirectStandardOutput $allLatinFile
 
-$upperFile = "$($outputDir)\five-letter-words-all-latin-upper.txt"
+$upperFile = "$($outputDir)/five-letter-words-all-latin-upper.txt"
 Start-Process $exe -NoNewWindow -Wait -ArgumentList 'toUpper' -RedirectStandardInput $allLatinFile -RedirectStandardOutput $upperFile
