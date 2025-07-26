f38ed308fcd585a4dc7e300b8bbd02b07926ee2e
Robert Impey <robert-impey@users.noreply.github.com>
2025-07-26 19:40:52 +0100

    5L: Remove some scaffolding

--- fivel/cmd/find5LetterWords.go
+++ fivel/cmd/find5LetterWords.go
@@ -34,14 +34,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(find5LetterWordsCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// find5LetterWordsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// find5LetterWordsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
