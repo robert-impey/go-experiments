b0fde2ae23307dc311ac74293eb77630eb78d0c3
Robert Impey <robert-impey@users.noreply.github.com>
2025-09-18 14:38:03 +0100

    LC: Convert Temperature

---
+++ converttemperature/converttemperature.go
@@ -0,0 +1,7 @@
+package converttemperature
+
+// https://leetcode.com/problems/convert-the-temperature/
+
+func convertTemperature(celsius float64) []float64 {
+	return []float64{celsius + 273.15, celsius*1.8 + 32}
+}
---
+++ converttemperature/converttemperature_test.go
@@ -0,0 +1,32 @@
+package converttemperature
+
+import (
+	"math"
+	"testing"
+)
+
+func TestConvertTemperature(t *testing.T) {
+	cases := []struct {
+		celsius  float64
+		expected []float64
+	}{
+		{36.50, []float64{309.65, 97.7}},
+		{122.11, []float64{395.26, 251.798}},
+	}
+
+	epsilon := 0.00001
+	for _, c := range cases {
+		got := convertTemperature(c.celsius)
+		if len(got) != len(c.expected) {
+			t.Errorf("convertTemperature(%f) != %v, got %v\n", c.celsius, c.expected, got)
+			continue
+		}
+		for i := range got {
+			err := math.Abs(got[i] - c.expected[i])
+			if err > epsilon {
+				t.Errorf("convertTemperature(%f) != %v, got %v\n", c.celsius, c.expected[i], got[i])
+				break
+			}
+		}
+	}
+}
